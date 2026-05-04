4fbfb90b5464682fc5db9f4f111be7af3610da13
BlairStChair <julia.z15@interia.pl>
2026-05-04 11:50:38 +0200

    Created component for page pattern

---
+++ src/app/components/pattern/pattern.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pattern',
+  imports: [],
+  templateUrl: './pattern.component.html',
+  styleUrl: './pattern.component.scss'
+})
+export class PatternComponent {
+
+}
